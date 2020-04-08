60783f103b4fcaa815835701cb76ffb55df713ae
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-04-08 02:39:24 -0300

    add test

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,12 +1,12 @@
 const connection = require('../database/connection'); // importando a conexão com o banco de dados
-const crypto = require('crypto'); // usaremos nesse contexto para criar uma id aleatória de uma ong
+const generateUniqueId = require('../utils/generateUniqueId'); // importando nossa função de gerar id's
 
 module.exports = {
     async create(request, response) {
         //const data = request.body; // dessa forma teríamos tudo o que voltasse do frontend, podendo conter informações desnecessárias
         const data = {name, email, whatsapp, city, uf} = request.body; // assim podemos garantir que só teremos as informações necessárias
 
-        const id = crypto.randomBytes(4).toString('HEX'); // nosso id será igual a 4 bytes de caracteres aleatórios convertidos para HEXADECIMAL
+        const id = generateUniqueId(); // nosso id será igual a 4 bytes de caracteres aleatórios convertidos para HEXADECIMAL
 
         await connection('ongs').insert({
             id,
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');// usaremos para tratar caminhos, rotas, url, etc
+const {celebrate, Segments, Joi} = require('celebrate'); // usaremos o celebrate para fazer a validação dos nossos dados
 
 const connection = require('./database/connection'); // importando a conexão com o banco de dados
 const routes = express.Router(); // modularizando nossas rotas para podermos exportá-las
@@ -12,14 +13,45 @@ const SessionsController = require('./controllers/SessionController'); // import
 routes.get('/ongs', OngController.list); // usando o método list da controller OngController para agir na rota /ongs com método get
 
 // rota usada para inserir uma nova ong
-routes.post('/ongs', OngController.create); // usando o método create da controller OngController para a gir na rota /ongs com método post
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create); // usando o método create da controller OngController para a gir na rota /ongs com método post
 
 
-routes.get('/incidents', IncidentController.list);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number().required()
+    })
+}), IncidentController.list);
 
-routes.get('/profile', ProfileController.list);
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required().max(120),
+        description: Joi.string().required().max(4000),
+        value: Joi.number().required()
+    }),
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), IncidentController.create);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfileController.list);
 
 routes.post('/sessions', SessionsController.create);
 
