81c2f2b189598a2ac4a7a90db97a3f9bf120dcae
Guilherme Oliveira <gui.passosdeoliveira@gmail.com>
2020-07-30 17:21:33 -0300

    add submodules || remove old frontend and backend folders

--- backend/src/server.js
+++
@@ -1,3 +0,0 @@
-const app = require('./app');
-
-app.listen(3333); // Nossa aplicação estará disponível na porta 3333
--- backend/src/utils/generateUniqueId.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto'); 
-
-module.exports = function generateUniqueId(){
-    return crypto.randomBytes(4).toString('HEX'); // nosso id será igual a 4 bytes de caracteres aleatórios convertidos para HEXADECIMAL
-}
